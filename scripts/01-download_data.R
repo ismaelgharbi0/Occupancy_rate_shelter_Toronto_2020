7ed09ae9f7238f1646f45425a9ac08480dfcc49c
ismaelgharbi0 <145019269+ismaelgharbi0@users.noreply.github.com>
2024-01-24 22:08:38 -0500

    Completed download_data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,38 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from the City of Toronto Open Data 
+# Portal about Shelter Occupancy in Toronto in 2020
+# Author: Ismael Gharbi 
+# Date: 23 January 2023 
+# Contact: ismael.gharbi@mail.utoronto.ca 
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites:Installing opendatatoronto package.
 
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# Code extracted from Open Data Toronto website
+
+# get package
+package <- show_package("shelter-occupancy-Toronto")
+package
+
+# get all resources for this package
+resources <- list_package_resources("shelter-occupancy-Toronto")
+
+# identify datastore resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# load the first datastore resource as a sample
+new_data <- filter(datastore_resources, row_number()==1) %>% get_resource()
+
+new_data
 
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
 
-         
+write_csv(new_data, "inputs/data/shelter_occupancy_2020.csv") 
